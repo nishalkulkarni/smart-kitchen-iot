a9e61863d1dff505c666da7efbbdbb258c078fe3
Nishal Kulkarni <nishalkulkarni@gmail.com>
2021-04-26 23:16:15 +0530

    Add db conf

--- api/routes/livefeed.js
+++ api/routes/livefeed.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.send("API is working properly");
+router.post("/", function (req, res, next) {
+  var data = req.body;
+
+  console.log(req.params);
+  console.log(req.body);
+  console.log(req.query);
+  res.send(`live POST received successfully: ${data}`);
 });
 
 module.exports = router;
