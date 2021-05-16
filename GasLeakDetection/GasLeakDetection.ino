ecda1ff9458d08f96a141c71fbb59f07f0b305b5
Nishal Kulkarni <nishalkulkarni@gmail.com>
2021-05-16 18:24:42 +0530

    Arduino code cleanup

--- GasLeakDetection/GasLeakDetection.ino
+++ GasLeakDetection/GasLeakDetection.ino
@@ -8,7 +8,7 @@
 
 #define FANPIN 10
 
-String server = "http://192.168.0.105:9000";
+String server = "http://192.168.1.3:9000";
 String uri = "/livefeed";
 
 SoftwareSerial esp8266(2, 3); // RX, TX
@@ -43,7 +43,7 @@ void setup() {
 
 void loop() {
     postData = "";
-    
+
     // Gas and Voltage
     updateGasVoltage();
 
@@ -51,32 +51,21 @@ void loop() {
     updateHumidityTemp();
     delay(2000);
 
-    if (esp8266.available()) { 
+    Serial.println(postData);
+    String postRequest = "POST " + uri + " HTTP/1.1\r\n" + "Host: " + server +
+                         "\r\n" + "Accept: *" + "/" + "*\r\n" +
+                         "Content-Length: " + postData.length() + "\r\n" +
+                         "Content-Type: application/json\r\n" + "\r\n{" +
+                         postData + "}\r\n";
+//    Serial.println(postRequest);
+                         
+
+
+    if (esp8266.available()) {
         Serial.println("esp is available");
         httppost(postData);
     }
     
-//    // TCP
-//    if (esp8266.available()) {
-//        if (esp8266.find("+IPD,")) {
-//            delay(1000);
-//            int connectionId = esp8266.read() - 48;
-//            esp8266.find("pin=");
-//            int pinNumber = (esp8266.read() - 48) * 10;
-//            pinNumber = pinNumber + (esp8266.read() - 48);
-//            int statusLed = (esp8266.read() - 48);
-//            digitalWrite(pinNumber, statusLed);
-//
-//            Serial.println(connectionId);
-//            Serial.print(pinNumber);
-//            Serial.print("      ");
-//            Serial.println(statusLed);
-//            String closeCommand = "AT+CIPCLOSE=";
-//            closeCommand += connectionId;
-//            closeCommand += "\r\n";
-//            sendData(closeCommand, 1000, DEBUG);
-//        }
-//    }
 }
 
 String sendData(String command, const int timeout, boolean debug) {
@@ -96,64 +85,79 @@ String sendData(String command, const int timeout, boolean debug) {
     return response; // return the String response.
 }
 
+void InitWifiModuleOLD()      
+{      
+  sendData("AT+RST\r\n", 2000, DEBUG);                                                  //reset the ESP8266 module.      
+//  delay(1000);      
+  sendData("AT+CWJAP=\"NishalEXT\",\"Nishal2000\"\r\n", 2000, DEBUG);        //connect to the WiFi network.      
+  delay (3000);      
+  sendData("AT+CWMODE=1\r\n", 1500, DEBUG);                                             //set the ESP8266 WiFi mode to station mode.      
+  delay (1000);      
+  sendData("AT+CIFSR\r\n", 1500, DEBUG);                                                //Show IP Address, and the MAC Address.      
+  delay (1000);      
+  sendData("AT+CIPMUX=1\r\n", 1500, DEBUG);                                             //Multiple conections.      
+  delay (1000);      
+  sendData("AT+CIPSERVER=1,80\r\n", 1500, DEBUG);                                       //start the communication at port 80, port 80 used to communicate with the web servers through the http requests.      
+}
+
 void InitWifiModule() {
     sendData("AT\r\n", 2000, DEBUG);
     sendData("AT+GMR\r\n", 2000, DEBUG);
-//    sendData("AT+UART_DEF=9600,8,1,0,0\r\n", 2000, DEBUG);
-//    sendData("AT+RST\r\n", 2000, DEBUG);
+//    sendData("AT+RESTORE\r\n", 2000, DEBUG);
+    sendData("AT+UART_DEF=9600,8,1,0,0\r\n", 2000, DEBUG);
+    sendData("AT+RST\r\n", 2000, DEBUG);
     delay(1000);
 
     // set the ESP8266 WiFi mode to station mode.
-    sendData("AT+CWMODE=1\r\n", 1500, DEBUG); 
+    sendData("AT+CWMODE=3\r\n", 1500, DEBUG);
     delay(2000);
-
-    sendData("AT+SLEEP=0\r\n", 1500, DEBUG); 
-    delay(2000);
-
-    sendData("AT+CWLAP\r\n", 6500, DEBUG); 
-    delay(6500);
-    
+//
+//        sendData("AT+CWLAP\r\n", 6500, DEBUG);
+//        delay(6500);
+//    
     // connect to the WiFi network.
-    sendData("AT+CWJAP=\"Nishal2.4\",\"Nishal2000\"\r\n", 6000, DEBUG);
+    sendData("AT+CWJAP=\"NishalEXT\",\"Nishal2000\"\r\n", 6000, DEBUG);
     delay(6000);
-    
+//    
     // Show IP Address, and the MAC Address.
-    sendData("AT+CIFSR\r\n", 3500, DEBUG); 
+    sendData("AT+CIFSR\r\n", 4500, DEBUG);
     delay(2000);
-    
+
     // Multiple conections.
-    sendData("AT+CIPMUX=1\r\n", 1500, DEBUG); 
+    sendData("AT+CIPMUX=1\r\n", 1500, DEBUG);
     delay(1000);
     
-//    sendData("AT+CIPSERVER=1,80\r\n", 1500, DEBUG);
+    // Show IP Address, and the MAC Address.
+    sendData("AT+CIFSR\r\n", 4500, DEBUG);
+    delay(2000);
 }
 
 void updateGasVoltage() {
-  // Gas Leak Detector
+    // Gas Leak Detector
     float sensorValue = analogRead(A0);
     float sensorVoltage = sensorValue / 1024 * 5.0;
 
-    postData += "gasvolt: "+ String(sensorVoltage) + ",";
+    postData += "gasvolt: " + String(sensorVoltage) + ",";
 
-    if (sensorVoltage > 3.6) {
-          // Buzzer
-          tone(PIEZOPIN, 1000, 5000);
-    
-          //    Fan simulator
-          digitalWrite(FANPIN, HIGH);
-          Serial.println("fan high");
-          delay(4000);
-          digitalWrite(FANPIN, LOW);
-          Serial.println("fan low");
-          delay(4000);
+    if (sensorVoltage > 2.5) {
+        // Buzzer
+        tone(PIEZOPIN, 1000, 5000);
+
+        //    Fan simulator
+        digitalWrite(FANPIN, HIGH);
+        Serial.println("fan high");
+        delay(4000);
+        digitalWrite(FANPIN, LOW);
+        Serial.println("fan low");
+        delay(4000);
     }
     delay(2000);
-
 }
 
 void updateHumidityTemp() {
     // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow
+    // sensor)
     float h = dht.readHumidity();
     // Read temperature as Celsius (the default)
     float t = dht.readTemperature();
@@ -167,7 +171,7 @@ void updateHumidityTemp() {
         float hic = dht.computeHeatIndex(t, h, false);
 
         postData += "temperature: " + String(t) + ",humidity: " + String(h) +
-                      ",heatindex: " + String(hic);
+                    ",heatindex: " + String(hic);
     }
 }
 
@@ -176,25 +180,25 @@ void httppost(String data) {
     String postRequest = "POST " + uri + " HTTP/1.1\r\n" + "Host: " + server +
                          "\r\n" + "Accept: *" + "/" + "*\r\n" +
                          "Content-Length: " + postData.length() + "\r\n" +
-                         "Content-Type: application/json\r\n" +
-                         "\r\n{" + postData + "}\r\n";
+                         "Content-Type: application/json\r\n" + "\r\n{" +
+                         postData + "}\r\n";
 
     // determine the number of caracters to be sent.
-    String sendCmd = "AT+CIPSEND="+String(postRequest.length())+"\r\n";
+    String sendCmd = "AT+CIPSEND=" + String(postRequest.length()) + "\r\n";
     Serial.print(sendCmd);
-    sendData(sendCmd, 2500, DEBUG); 
+    sendData(sendCmd, 2500, DEBUG);
     delay(500);
-        
-//    esp8266.print(sendCmd);
-//    esp8266.println(postRequest.length());
 
+    //    esp8266.print(sendCmd);
+    //    esp8266.println(postRequest.length());
 
-//        Serial.println(postRequest);
-        Serial.println(postData);
+    //        Serial.println(postRequest);
+    Serial.println(postData);
 
     if (esp8266.find(">")) {
         Serial.println("Sending..");
-        esp8266.print(postRequest);
+        //        esp8266.print(postRequest);
+        sendData(postRequest, 3500, DEBUG);
     }
     delay(3000);
 
@@ -203,7 +207,7 @@ void httppost(String data) {
     }
 
     delay(3000);
-    
+
     while (esp8266.available()) {
         String tmpResp = esp8266.readString();
         Serial.println(tmpResp);
