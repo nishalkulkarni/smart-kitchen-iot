182e23b6bc70a7cb4801be28ca2e8a97480c8387
Nishal Kulkarni <nishalkulkarni@gmail.com>
2021-11-06 23:59:47 +0530

    more inventory management features

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -2,15 +2,25 @@ import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 import RecipeCard from "./RecipeCard";
 
 import useFirestore from "../hooks/useFirestore";
+import { projectFirestore } from '../firebase/config';
 
 export default function Inventory(props) {
   const [ingred, setIngred] = useState([]);
   const [recipeData, setRecipeData] = useState(null);
   const [lastSynch, setLastSynch] = useState(null);
+  const [totalValue, setTotalValue] = useState(null);
+  const [openPopup, setOpenPopup] = useState(false);
+  const [onlineItem, setOnlineItem] = useState(false);
 
   //Pulls the documents in collection called 'inventory'
   const { docs } = useFirestore("inventory");
@@ -22,12 +32,24 @@ export default function Inventory(props) {
         return item.produce.name;
       })
     );
-    console.log(ingred);
   }, [docs]);
 
   async function getAvailableRecipes() {
     getRecipeByIng();
   }
+
+  const handlePopupOpen = (itemname) => {
+    setOnlineItem(itemname);
+    setOpenPopup(true);
+  };
+
+  const handlePopupClose = () => {
+    setOpenPopup(false);
+  };
+
+  const buyOnline = () => {
+    window.open("https://www.amazon.in/s?k="+onlineItem, "_blank")
+  };
 
   function getRecipeByIng() {
     let ingredQuery = "";
@@ -56,10 +78,78 @@ export default function Inventory(props) {
       });
   }
 
+  async function getInventoryValue() {
+    let totalValue = 0
+    docs.forEach((item) => {
+      totalValue += item.produce.price;
+    })
+
+    setTotalValue(totalValue);
+  }
+
+  async function removeItem(e, item) {
+    projectFirestore.collection("inventory").doc(item.id).delete().then(() => {
+      console.log("Item successfully deleted!");
+    }).catch((error) => {
+      console.log("Error removing item: ", error);
+    });
+  }
+
+  async function updateQuantity(e, increase, item) {
+    let val = item.produce.value;
+    if (increase) {
+      val += 1;
+    } else {
+      val -= 1;
+    }
+    if (val < 0)
+      val = 0;
+
+    if (!item.produce.stockreminder && val == 0) {
+      projectFirestore.collection("inventory").doc(item.id).delete().then(() => {
+        console.log("Item successfully deleted!");
+      }).catch((error) => {
+        console.log("Error removing item: ", error);
+      });
+    } else {
+      projectFirestore.collection("inventory").doc(item.id).update({
+        produce: {
+          name: item.produce.name,
+          unit: item.produce.unit,
+          value: val,
+          image: item.produce.image,
+          stockreminder: item.produce.stockreminder,
+          price: item.produce.price,
+        },
+      });
+
+      if (item.produce.stockreminder && val == 0) {
+        // alert(item.produce.name + "Out of stock!");
+        handlePopupOpen(item.produce.name);
+      }
+    }
+
+
+  }
+
+  async function setStockReminder(e, enable, item) {
+    projectFirestore.collection("inventory").doc(item.id).update({
+      produce: {
+        name: item.produce.name,
+        unit: item.produce.unit,
+        value: item.produce.value,
+        image: item.produce.image,
+        stockreminder: enable,
+        price: item.produce.price,
+      },
+    });
+  }
+
   const inventoryList = {
     backgroundColor: "rgba(245,255,253,0.5)",
     overflowY: "scroll",
-    minHeight: "400px",
+    minHeight: "420px",
+    padding: "16px",
     display: "flex",
     flexDirection: "row",
     backgroundPosition: "center",
@@ -75,7 +165,7 @@ export default function Inventory(props) {
         className="sbi-label"
         style={{ backgroundColor: "white", borderRadius: "15px" }}
       >
-        Last Synched to{" "}
+        Last Synced to{" "}
         <img
           src="https://firebase.google.com/downloads/brand-guidelines/PNG/logo-standard.png"
           width="71px"
@@ -84,29 +174,65 @@ export default function Inventory(props) {
         : {lastSynch}
       </h2>
       <div style={{ boxShadow: "5px", border: "2px solid black" }}>
-        <GridList cellHeight={160} cols={3} style={inventoryList}>
+        <GridList cellHeight={380} cols={4} gap={20} style={inventoryList}>
           {docs.map((item) => (
-            <div className="invlist-div">
-              <img
-                src={item.produce.image}
-                width="200px"
-                style={{ marginTop: "20px" }}
-              />
-              <GridListTile>
-                {" "}
-                <h2 className="invlist-label">{item.produce.name}</h2>{" "}
-                <b>
-                  {item.produce.value} {item.produce.unit}
-                </b>{" "}
-              </GridListTile>
-            </div>
+            <GridListTile>
+              <div className="invlist-div">
+                <img
+                  src={item.produce.image}
+                  width="200px"
+                  style={{ marginTop: "20px" }}
+                />
+                <div>
+                  {" "}
+                  <h2 className="invlist-label">{item.produce.name}</h2>{" "}
+                  <b>
+                    {"Rs"} {item.produce.price}
+                  </b>{" "}
+                  <br />
+                  <b style={{ fontSize: "20px" }}>
+                    {item.produce.value} {item.produce.unit}
+                  </b>{" "}
+                </div>
+                <div>
+                  <button className="button-info" onClick={(e) => updateQuantity(e, true, item)}>
+                    ➕
+                  </button>
+                  <button className="button-info" onClick={(e) => updateQuantity(e, false, item)}>
+                    ➖
+                  </button>
+                  <button className="button-danger" onClick={(e) => removeItem(e, item)}>
+                    Remove Item
+                  </button>
+                  {item.produce.stockreminder &&
+                    <button className="button-default" onClick={(e) => setStockReminder(e, false, item)}>
+                      🔕
+                    </button>
+                  }
+                  {!item.produce.stockreminder &&
+                    <button className="button-default" onClick={(e) => setStockReminder(e, true, item)}>
+                      🔔
+                    </button>
+                  }
+
+                </div>
+              </div>
+            </GridListTile>
           ))}
         </GridList>
       </div>
       <div>
         <button className="button-default" onClick={getAvailableRecipes}>
-          Available Recipes
+          View Available Recipes
         </button>
+        <button className="button-default" onClick={getInventoryValue}>
+          Get Inventory Value
+        </button>
+      </div>
+      <div className="inventory-val-cards-div">
+        {totalValue &&
+          <h2><small>Total Value of inventory items is:</small> Rs {totalValue}</h2>
+        }
       </div>
       <div className="recipe-cards-div">
         {recipeData &&
@@ -123,6 +249,27 @@ export default function Inventory(props) {
             );
           })}
       </div>
+      <Dialog
+        open={openPopup}
+        onClose={handlePopupClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {onlineItem + " Out of Stock!"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            You have run out of {onlineItem}. You can purchase more using the buy link below.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handlePopupClose}>Close</Button>
+          <Button onClick={buyOnline} autoFocus>
+            Buy on Amazon
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Grid>
   );
 }
